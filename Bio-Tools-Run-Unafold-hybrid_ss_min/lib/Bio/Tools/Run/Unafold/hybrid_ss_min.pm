993006df6825eedb34c2bc281c9aaa87c615e0f0
cassj <cassjohnston@gmail.com>
2010-12-23 13:44:05 +0000

    started on hybrid-ss-min

--- Bio-Tools-Run-Unafold-hybrid_ss_min/lib/Bio/Tools/Run/Unafold/hybrid_ss_min.pm
+++ Bio-Tools-Run-Unafold-hybrid_ss_min/lib/Bio/Tools/Run/Unafold/hybrid_ss_min.pm
@@ -19,81 +19,123 @@ use strict;
 use warnings;
 package Bio::Tools::Run::Unafold::hybrid_ss_min;
 
-use base 'Bio::Tools::Run::Unafold::Base';
+use base 'Bio::Tools::Run::WrapperBase::Accessor';
+
+use File::Spec;
+use Path::Class;
+
+BEGIN {
+
+  __PACKAGE__->_register_parameters( 
+      {
+       NA          => 'Nucleic Acid. RNA | DNA. Default is RNA',
+       tmin        => 'Minimum Temperature. Default is 0',
+       tinc        => 'Temperature Increment. Default is 1',
+       tmax        => 'Maximum Temperature. Default is 100',
+       sodium      => 'Sodium ion concentration (molar). Default is 1',
+       magnesium   => 'Magnesium ion concentration (molar). Default is 0',
+       polymer     => 'Use salt corrections for polymers instead of oligomers. Boolean. Default is 0',
+       #suffix      => 'Use energy rules with the given suffix. Default is ""',
+       output      => 'Name output files with the given string as a prefix. Default is ""',
+       prohibit    => 'Prohibit all basepairs in the helix from i,j to i+k-1,j-k+1. If j is 0, prohibit bases i to i+k-1 from pairing at all; if i is 0, prohibit bases j to j-k+1 from pairing at all. k defaults to 1',
+       force       => 'Force all basepairs in the helix from i,j to i+k-1,j-k+1. If j is 0, forces bases i to i+k-1 to be double-stranded; if i is 0, forces bases j to j-k+1 to be double-stranded. k defaults to 1',
+       energyOnly  => 'Skips computation of probabilities. Boolean',
+       noisolate   => 'Prohibit all isolated basepairs. Isolated basepairs are helices of length 2; that is, they do not stack on another basepair on either side. Boolean',
+       mfold       => '[P,W,MAX], perform multiple (suboptimal) tracebacks in the style of mfold. P indicates the percent suboptimality to consider; only structures with energies within P% of the minimum will be output. W indicates the window size; a structure must have at least W basepairs that are each a distance of at least W away from any basepair in a previous structure. MAX represents an absolute limit on the number of structures computed.',
+       zip         => 'Force zipping up of helices by forcing single-stranded bases to dangle on adjacent basepairs when possible. Boolean',
+       tracebacks  => 'Computes the given number of stochastic tracebacks. Computed according to the Boltzmann probability distribution so that the probability of a structure is its Boltzmann factor divided by the partition function',
+       maxbp       => 'Bases further apart than the specified number cannot form. Default is no limit',
+       allpairs    => 'Allows basepairs to form between any two nucleotides. Watson-Crick and wobble are default',
+       maxloop     => 'Maximum size of bulge/interior loops. Default is 30',
+       nodangle    => 'Removes single-base stacking from consideration. Boolean',
+       simple      => 'Makes the penalty for multibranch loops constant rather than affine. Boolean',
+       prefilter   => 'Filter our all basepairs except those in groups of ',
+       circular    => 'treat sequences as circular rather than linear',
+      });
+
+} # BEGIN
 
 
-=head2 parameters
+=head2 new
 
-Title    : parameters
-Usage    : Accessor
-Function : Returns a list of valid parameters and descriptions.
-Returns  : HashRef in the form {paramname => 'description', ... }
-Args     : None
+  Title    : new
+  Usage    : my $hss = Bio::Tools::Run::Unafold::hybrid_ss_min->new();
+  Function : Constructor
+  Returns  : An object of class Bio::Tools::Run::Unafold::hybrid_ss_min
+  Args     : Any of the command line parameters can be passed to the 
+             constructor. It will also accept -program_dir
 
 =cut
 
-
-BEGIN { 
-  my $params = {
-		NA          => 'Nucleic Acid. RNA | DNA. Default is RNA',
-		tmin        => 'Minimum Temperature. Default is 0',
-		tinc        => 'Temperature Increment. Default is 1',
-		tmax        => 'Maximum Temperature. Default is 100',
-		sodium      => 'Sodium ion concentration (molar). Default is 1',
-		magnesium   => 'Magnesium ion concentration (molar). Default is 0',
-		polymer     => 'Use salt corrections for polymers instead of oligomers. Boolean. Default is 0',
-		#suffix      => 'Use energy rules with the given suffix. Default is ""',
-		output      => 'Name output files with the given string as a prefix. Default is ""',
-		prohibit    => 'Prohibit all basepairs in the helix from i,j to i+k-1,j-k+1. If j is 0, prohibit bases i to i+k-1 from pairing at all; if i is 0, prohibit bases j to j-k+1 from pairing at all. k defaults to 1',
-		force       => 'Force all basepairs in the helix from i,j to i+k-1,j-k+1. If j is 0, forces bases i to i+k-1 to be double-stranded; if i is 0, forces bases j to j-k+1 to be double-stranded. k defaults to 1',
-		energyOnly  => 'Skips computation of probabilities. Boolean',
-		noisolate   => 'Prohibit all isolated basepairs. Isolated basepairs are helices of length 2; that is, they do not stack on another basepair on either side. Boolean',
-		mfold       => '[P,W,MAX], perform multiple (suboptimal) tracebacks in the style of mfold. P indicates the percent suboptimality to consider; only structures with energies within P% of the minimum will be output. W indicates the window size; a structure must have at least W basepairs that are each a distance of at least W away from any basepair in a previous structure. MAX represents an absolute limit on the number of structures computed.',
-		zip         => 'Force zipping up of helices by forcing single-stranded bases to dangle on adjacent basepairs when possible. Boolean',
-		tracebacks  => 'Computes the given number of stochastic tracebacks. Computed according to the Boltzmann probability distribution so that the probability of a structure is its Boltzmann factor divided by the partition function',
-		maxbp       => 'Bases further apart than the specified number cannot form. Default is no limit',
-		allpairs    => 'Allows basepairs to form between any two nucleotides. Watson-Crick and wobble are default',
-		maxloop     => 'Maximum size of bulge/interior loops. Default is 30',
-		nodangle    => 'Removes single-base stacking from consideration. Boolean',
-		simple      => 'Makes the penalty for multibranch loops constant rather than affine. Boolean',
-		prefilter   => 'Filter our all basepairs except those in groups of ',
-		circular    => 'treat sequences as circular rather than linear',
-		stream      => 'read sequences from STDIN rather than a file. Implies quiet.',
-		quiet       => 'In quiet mode hybrid-ss-min interprets the arguments on the command line as sequences themselves, rather than as names of files containing sequences. In addition, no files are written and standard output consists only of the free energies.'
-	       }
-
-    __PACKAGE__->_mk_param_accessors($params);
-
-  } # BEGIN
-
-
-sub new{
+sub new {
   my $class = shift;
-
-  # Use the Bio::Root::Root constructor from WrapperBase
-  my $self = $class->SUPER->new(@_);
-  
-  # initialise our parameters with passed values. 
-  # Bioperl insists on using this _rearrange thing.
-  # which is just a pita if you ask me, but meh.
-  
+  my $self = $class->SUPER::new(@_); 
   bless $self, $class;
+
+  my ($pd, $pn) =  $self->_rearrange(['program_dir', 'program_name'], @_);
+  $pd = $ENV{UNAFOLD_DIR} unless $pd;
+  $self->{program_dir} = $pd if $pd;
+
+  $self->{program_name} = $pn || 'hybrid-ss-min';
+
+  return $self;
+
+}
+
+=head2 program_name
+
+  Title    : program_name
+  Usage    : $folder->program_name
+  Function : returns the name of the executable
+  Returns  : string
+  Args     : none
+
+=cut
+
+sub program_name{
+  my $self = shift;
+  return $self->{program_name};
+}
+  
+
+=head2 program_dir
+  
+  Title    : program_dir
+  Usage    : my $path = $folder->program_dir;
+  Function : Returns the current program_dir, the location in which 
+             the Unafold executables can be found, if one is defined.
+             You can define the program_dir by setting the 
+             $UNAFOLD_DIR environment variable, or by passing a path
+             to the -program_dir parameter of ->new().
+             If left undefined, the system $PATH will be searched for
+             the executables.
+  Returns  : A path, or undef
+  Args     : none
+
+=cut
+
+sub program_dir{
+    my $self = shift;   
+    return $self->{program_dir};
 }
 
 
-# this is what the Class::Accessor constructor does
-# personally I don't like the whole $proto thing - I don't
-# think that you *should* be able to create new instances
-# from existing ones. 
-#sub new {
-#    my($proto, $fields) = @_;
-#    my($class) = ref $proto || $proto;
-#
-#    $fields = {} unless defined $fields;
-#
-#    # make a copy of $fields.
-#    bless {%$fields}, $class;
-#}
+=head2 version
+
+  Title    : version
+  Usage    : $folder->version;
+  Function : returns the hybrid-ss-min version
+  Returns  : A string
+  Args     : None
+
+=cut
+
+sub version{
+    my $self = shift;
+    warn "not implemented yet";
+    return;
+}
+
 
 
 =head1 FEEDBACK
--- Bio-Tools-Run-Unafold-hybrid_ss_min/t/01.t
+++ Bio-Tools-Run-Unafold-hybrid_ss_min/t/01.t
@@ -1,15 +1,41 @@
+# Define $UNAFOLD_DIR if unafold executables are not in your path
+# alter $exe if it isn't called hybrid-ss-min
+
 use strict;
 use warnings;
 
 use Bio::Root::Test;
+test_begin(-tests => 15);
+my $debug = test_debug();
 
-test_begin(-tests => 1,
-           -requires_modules => [qw()],
-           -requires_networking => 0);
+use_ok( 'Bio::Tools::Run::Unafold::hybrid_ss_min' ); 
+require_ok( 'Bio::Tools::Run::Unafold::hybrid_ss_min' );
 
-is(1,1,"an example test");
+my $exe = 'hybrid-ss-min';
+
+my $folder = Bio::Tools::Run::Unafold::hybrid_ss_min->new(
+		      -verbose => $debug, 
+		      -program_name => $exe
+							 );
+isa_ok($folder, 'Bio::Tools::Run::Unafold::hybrid_ss_min');
 
 
+can_ok($folder, 'parameters');
+can_ok($folder, 'valid_parameters');
+can_ok($folder,  'is_valid_parameter');
+can_ok($folder, 'NA');
+can_ok($folder, 'tmax');
+can_ok($folder, 'prohibit');
 
+can_ok($folder, 'program_dir');
+is($folder->program_dir, undef);
 
+TODO: {
+  local $TODO = 'version not implemeted yet';
+  can_ok($folder, 'version');
+  is($folder->version, 'version number');
+}
+
+can_ok($folder, 'executable');
+isnt($folder->executable, '');
 
